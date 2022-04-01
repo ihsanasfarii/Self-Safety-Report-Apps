d0c52177d59c7f5d022de798f8fee4e5dd3534fd
Ihsanasfari <ihsanasfari@student.telkomuniversity.ac.id>
2022-04-01 19:00:35 +0700

    nge rapihin

--- lib/Profile/ProfilePage.dart
+++ lib/Profile/ProfilePage.dart
@@ -4,31 +4,24 @@ import 'package:selfsafetyapp_test/navbar.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-  bottomNavigationBar: MyBottomNavBar(),
-      body: Center(
-child: Container(
-transform: Matrix4.translationValues(0.0, 250.0, 0.0),
-child: Stack(
-children: <Widget>[
-Container(
-width: 250,
-height: 650,
-color: Colors.white, 
-),
-Container(
-padding: const EdgeInsets.all(6.0),
-alignment: Alignment.bottomCenter,
-decoration: BoxDecoration(
-  color:  Color.fromARGB(255, 131, 137, 218), borderRadius: BorderRadius.circular(40)),
-        )
-        
-        ] 
-)
-)
-)
-);
-          }
+        bottomNavigationBar: MyBottomNavBar(),
+        body: Center(
+            child: Container(
+                transform: Matrix4.translationValues(0.0, 250.0, 0.0),
+                child: Stack(children: <Widget>[
+                  Container(
+                    width: 250,
+                    height: 650,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(6.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 131, 137, 218),
+                        borderRadius: BorderRadius.circular(40)),
+                  )
+                ]))));
+  }
 }

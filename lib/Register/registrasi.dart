23646c90adc5ae1b35b029a0670cee0820875c0e
izalmfrds <rizalmaidanytb@gmail.com>
2022-04-07 00:39:50 +0700

    dropdownv2

--- lib/Register/registrasi.dart
+++ lib/Register/registrasi.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 
 class registrasi extends StatelessWidget {
+  String dropdownvalue = '1';
+
+  var items = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
+    '25',
+    '26',
+    '27',
+    '28',
+    '29',
+    '30',
+    '31'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,10 +184,42 @@ class registrasi extends StatelessWidget {
                 SizedBox(
                   height: 8,
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'dd/mm/yyyy',
-                    
+                Column(
+                  children: [
+                    DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        focusColor: Colors.green,
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 206, 204, 204),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(255, 0, 0, 0))),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(255, 0, 0, 0))),
+                      ),
+                      value: dropdownvalue,
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),
+
+                      // Array list of items
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      // After selecting the desired option,it will
+                      // change button value to selected value
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                        });
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -203,3 +271,5 @@ class registrasi extends StatelessWidget {
     );
   }
 }
+
+void setState(Null Function() param0) {}

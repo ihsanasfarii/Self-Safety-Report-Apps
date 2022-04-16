74f1b03c07a5f993e6f26998e46782a25963e0b3
adamnn26 <79630124+adamnn26@users.noreply.github.com>
2022-04-16 12:58:29 +0700

    update halaman pesan

--- lib/Message/pesan.dart
+++ lib/Message/pesan.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:selfsafetyapp_test/Message/list_pesan.dart';
+import 'package:selfsafetyapp_test/navbar.dart';
+import 'package:selfsafetyapp_test/sumber.dart';
 
 class pesan extends StatefulWidget {
   const pesan({Key? key}) : super(key: key);
@@ -11,13 +14,144 @@ class _pesanState extends State<pesan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/halamanchat.png'),
+            image: AssetImage("assets/halamanchat.png"),
             fit: BoxFit.cover,
           ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Chats",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.pink[50],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.add,
+                            color: Colors.pink,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            "New",
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey.shade400),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade400,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Colors.grey.shade100),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: chatsData.length,
+              itemBuilder: (context, index) => ChatCard(chat: chatsData[index]),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    Key? key,
+    required this.chat,
+  }) : super(key: key);
+
+  final Chat chat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
+      child: Row(
+        children: [
+          Stack(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(chat.image),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    chat.name,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 8),
+                  Opacity(
+                    opacity: 0.64,
+                    child: Text(
+                      chat.lastMessage,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: 0.64,
+            child: Text(chat.time),
+          ),
+        ],
       ),
     );
   }
--- lib/sumber.dart
+++ lib/sumber.dart
@@ -5,3 +5,8 @@ const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 20.0;
+const kSecondaryColor = Color(0xFFFE9901);
+const kContentColorLightTheme = Color(0xFF1D1D35);
+const kContentColorDarkTheme = Color(0xFFF5FCF9);
+const kWarninngColor = Color(0xFFF3BB1C);
+const kErrorColor = Color(0xFFF03738);

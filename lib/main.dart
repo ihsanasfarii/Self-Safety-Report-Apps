3e8d803f4e977d3a8c4f00a17dea76aefc76bb9f
adjierizqan <79629442+adjierizqann@users.noreply.github.com>
2022-06-09 20:05:27 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:selfsafetyapp_test/Beranda/beranda.dart';
+import 'package:selfsafetyapp_test/Login/InputField.dart';
 import 'package:selfsafetyapp_test/Message/pesan.dart';
 import 'Popup/popupmessage.dart';
 import 'Register/registrasi.dart';
@@ -9,6 +10,7 @@ import 'Beranda/beranda.dart';
 import 'Profile/ProfilePage.dart';
 import 'Message/pesan.dart';
 import 'package:flutter_responsive/flutter_responsive.dart';
+import 'Profile/FotoProfile.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,9 +27,12 @@ class MyApp extends StatelessWidget {
         '/LoginPage': (context) => LoginPage(),
         '/register': (context) => registrasi(),
         '/beranda': (context) => beranda(),
-        '/ProfilePage': (context) => ProfilePage(),
+        '/ProfilePage': (context) => ProfilePage(title: ""),
         '/pesan': (context) => pesan(),
         '/popupmessage': (context) => popupmessage(),
+        '/FotoProfile': (context) => ayam(
+              Email: '',
+            ),
       },
     );
   }
